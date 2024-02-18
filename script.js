ddead0814e6295df6870a6c491803fad5e87a7f4
Paul Maxwell <paulcmaxwell@gmail.com>
2024-02-18 11:15:07 -0500

    Fourth commit

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ const updateDimensions = function () {
   let ih = window.innerHeight;
   let ow = window.outerWidth;
   let oh = window.outerHeight;
-  inner.textContent = `${iw}px x ${ih}px`;
-  outer.textContent = `${ow}px x ${oh}px`;
+  inner.textContent = `${iw} x ${ih}`;
+  outer.textContent = `${ow} x ${oh}`;
 };
 
 updateDimensions();
